48437039767d2e665360b825371706f3e690740f
abhyuday25 <sinhaabhyuday25@gmail.com>
2026-03-29 20:12:29 +0530

    2nd commit

---
+++ src/jenkin/Main.java
@@ -0,0 +1,10 @@
+package jenkin;
+
+public class Main {
+public static void main(String[] args) {
+System.out.println("NAME -- Abhyuday Sinha");
+System.out.println("REGISTRATION NUMBER -- 23MIC0036");
+System.out.println("Java program executed using Jenkins CI");
+System.out.println("Jenkins-GitHUB Integration Successful");
+}
+}
